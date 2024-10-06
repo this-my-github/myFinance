6beee73f7ff3ca44e5671504aceaa722eefa4cc6
SergeyMankevich <this_mail_for_everything@mail.ru>
2024-10-06 20:38:13 +0300

    add Docker

---
+++ README.md
@@ -0,0 +1,21 @@
+# Приложение Мои финансы
+
+## - про учёт расходов и доходов
+
+## Страницы:
+
+### -Главная
+
+### -История;
+
+### -Категории;
+
+### -Авторизация;
+
+### -Регистрация;
+
+### -Смена пароля;
+
+## Используемые технологии:
+
+### vite, Styled Components, React Hook Form и Yup, React Router, Redux, Redux Thunk, React Redux, Node, Express, MongoDb, Mongoose, JWT, Bcrypt
--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 
+const path = require("path");
 const express = require("express");
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
@@ -19,7 +20,13 @@ app.use(cors(corsOptions));
 app.use(cookieParser());
 app.use(express.json());
 
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
 app.use("/api", routes);
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+});
 
 mongoose.connect(process.env.DB_CONNECTION_STRING).then(() => {
   app.listen(port, () => {
